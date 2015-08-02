c95ec3c970f4ff08539e255316241ab5d2cd8187
Karan Talreja <karantalreja91@gmail.com>
2015-08-02 22:11:49 +0530

    Added support for buit-in types generic heap
    Heap uses traits class now to determine the how the data is to be
    sorted.

--- apps/heapTest/src/main.cpp
+++ apps/heapTest/src/main.cpp
@@ -5,17 +5,16 @@
  *      Author: karan
  */
 #include <heap.hpp>
-#include <edge.hpp>
+#include <type_traits>
 
 int main()
 {
-	heap <edge<size_t> > b;
-	b.insert(*(new edge<size_t>()));
-	b.insert(*(new edge<size_t>()));
-	b.eval();
-	heap <edge<int> > c;
-	c.insert(*(new edge<int>()));
-	c.insert(*(new edge<int>()));
-	c.eval();
+	heap<size_t,basic_heap_min_traits<size_t> > a;
+	for (size_t i = 1; i <= 10 ; i++) a.insert(i);
+	a.decompile();
+	a.extractTop();
+	a.decompile();
+	a.insert(1);
+	a.decompile();
 	return 0;
 }
--- utils/heap/include/heap.hpp
+++ utils/heap/include/heap.hpp
@@ -10,49 +10,116 @@
 
 #include <iostream>
 #include <vector>
+#include <type_traits>
 using namespace std;
 
-template<class keyT, class actual_heap_node>
-class basic_heap_node
+template<class T>
+class basic_heap_traits
 {
 public:
-	keyT actual_heap_node::*keyAccessor;
-	basic_heap_node()
+	static bool eq (T& a, T& b);
+	static bool lt (T& a, T& b);
+};
+
+template <class T, bool isFundamental = std::is_fundamental< typename std::remove_pointer<T>::type >::value > class basic_heap_min_traits;
+template <class T, bool isFundamental = std::is_fundamental< typename std::remove_pointer<T>::type >::value > class basic_heap_max_traits;
+
+template <class T>
+class basic_heap_min_traits<T,true> : public basic_heap_traits <T>
+{
+public:
+	static bool eq (T& a, T& b) {return a == b ? true : false;}
+	static bool lt (T& a, T& b) {return a < b ? true : false;}
+	static bool lte (T& a, T& b)
 	{
-		keyAccessor = NULL;
-	}
-	basic_heap_node (keyT actual_heap_node::*thatKeyAccessor)
-	{
-		keyAccessor = thatKeyAccessor;
-	}
-	keyT applyKeyAccessor()
-	{
-		return (NULL != keyAccessor) ? static_cast<actual_heap_node*>(this)->*keyAccessor : keyT();
+		if (true == basic_heap_min_traits<T>::eq(a,b)) return true;
+		return basic_heap_min_traits<T>::lt(a,b);
 	}
 };
 
 template <class T>
-class heap
+class basic_heap_max_traits<T,true> : public basic_heap_traits <T>
 {
 public:
-	vector<T> _heap;
-	void insert (T node)
+	static bool eq (T& a, T& b) {return a == b ? true : false;}
+	static bool lt (T& a, T& b) {return a > b ? true : false;}
+	static bool lte (T& a, T& b)
 	{
-		_heap.push_back(node);
-	}
-
-	void eval ()
-	{
-		while (!_heap.empty())
-		{
-			cout << _heap.back() << comp(_heap.back()) << endl;
-			_heap.pop_back();
-		}
-	}
-	bool comp(T node)
-	{
-		return (node.applyKeyAccessor());
+		if (true == basic_heap_max_traits<T>::eq(a,b)) return true;
+		return basic_heap_max_traits<T>::lt(a,b);
 	}
 };
+
+template <class T, class traits = basic_heap_traits<T> >
+class heap
+{
+	vector<T> m_heap;
+	size_t m_size;
+public:
+	heap()
+	{
+		m_size=0;
+	}
+	void insert(T);
+	T extractTop();
+	void decompile();
+	size_t getSize()
+	{return m_size;}
+};
+
+template <class T, class traits>
+void heap<T,traits>::insert(T node)
+{
+	size_t parent,child;
+	T tempNode;
+	m_heap.push_back(node);
+	m_size++;
+	child = m_size;
+	parent = (child)/2;
+	while((parent >=1) && (false == traits::lt(m_heap[parent-1],m_heap[child-1])))
+	{
+		tempNode = m_heap[child-1];
+		m_heap[child-1] = m_heap[parent-1];
+		m_heap[parent-1] = tempNode;
+		child = parent;
+		parent = parent/2;
+	}
+}
+
+template <class T, class traits>
+T heap<T,traits>::extractTop(void)
+{
+	T retVal = m_heap[0],tempNode;
+	tempNode = m_heap[0];
+	m_heap[0] = m_heap[m_size-1];
+	m_heap[m_size-1] = tempNode;
+	typename vector<T>::iterator itr = m_heap.begin();
+	itr = itr + m_size -1;
+	m_heap.erase(itr);
+	m_size--;
+	int parent = 1;
+	int child = parent;
+	while(2*parent < m_size)
+	{
+		child = (true == traits::lte(m_heap[(2*parent)-1],m_heap[(2*parent)])) ? 2*parent : 2*parent + 1;
+		if(false == traits::lt(m_heap[parent-1],m_heap[child-1]))
+		{
+			tempNode = m_heap[child -1];
+			m_heap[child - 1] = m_heap[parent - 1];
+			m_heap[parent - 1] = tempNode;
+			parent = child;
+		}
+		else
+			break;
+	}
+	return retVal;
+}
+
+template <class T, class traits>
+void heap<T,traits>::decompile()
+{
+	for(typename vector<T>::iterator itr = m_heap.begin(); itr != m_heap.end() ;itr++) cout<< *itr <<" ";
+	cout<<endl;
+}
 
 #endif /* UTILS_HEAP_INCLUDE_HEAP_HPP_ */
