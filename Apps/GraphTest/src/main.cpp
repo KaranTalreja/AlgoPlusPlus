d75d2715cc3538a9c82eafc29af04f4580b8215b
Karan Talreja <karantalreja91@gmail.com>
2015-08-10 00:28:29 +0530

    Added graph<a*,b*>, <a*,b> <a,b*> template specializations

--- Apps/GraphTest/src/main.cpp
+++ Apps/GraphTest/src/main.cpp
@@ -40,17 +40,49 @@ struct edge
 };
 
 int main() {
-	graph <node,edge> a(500);
 	node firstNode(1);
 	node secondNode(2);
 	edge tempEdge(&firstNode,&secondNode,10);
 	edge revTempEdge(&secondNode,&firstNode,-10);
+
+	//Both normal input templates
+	graph <node,edge> a(500);
 	a[1] = firstNode;
 	a[2] = secondNode;
 	a[1].addEdge(tempEdge);
 	a[2].addEdge(revTempEdge);
 	using graphNodeDesc = graph<node,edge>::nodeDescriptorType;
 	graphNodeDesc v;
-	cout << a << endl << a[1][v].m_id <<" "<< a[2][v].m_id <<" "<< a[1][1].m_weight <<" "<< a[2][1].m_weight << endl;
+	cout << a << a[1][v].m_id <<" "<< a[2][v].m_id <<" "<< a[1][1].m_weight <<" "<< a[2][1].m_weight << endl <<endl;
+
+	//Both pointer input templates
+	graph <node*,edge*> b(500);
+	b[1] = &firstNode;
+	b[2] = &secondNode;
+	b[1].addEdge(&tempEdge);
+	b[2].addEdge(&revTempEdge);
+	using graphNodeDesc1 = graph<node*,edge*>::nodeDescriptorType;
+	graphNodeDesc1 v1;
+	cout << b << b[1][v1]->m_id <<" "<< b[2][v1]->m_id <<" "<< b[1][1]->m_weight <<" "<< b[2][1]->m_weight << endl <<endl;
+
+	//node pointer input template only
+	graph <node*,edge> c(500);
+	c[1] = &firstNode;
+	c[2] = &secondNode;
+	c[1].addEdge(tempEdge);
+	c[2].addEdge(revTempEdge);
+	using graphNodeDesc2 = graph<node*,edge>::nodeDescriptorType;
+	graphNodeDesc2 v2;
+	cout << c << c[1][v2]->m_id <<" "<< c[2][v2]->m_id <<" "<< c[1][1].m_weight <<" "<< c[2][1].m_weight << endl <<endl;
+
+	//edge pointer input template only
+	graph <node,edge*> d(500);
+	d[1] = firstNode;
+	d[2] = secondNode;
+	d[1].addEdge(&tempEdge);
+	d[2].addEdge(&revTempEdge);
+	using graphNodeDesc3 = graph<node,edge*>::nodeDescriptorType;
+	graphNodeDesc3 v3;
+	cout << d << d[1][v3].m_id <<" "<< d[2][v3].m_id <<" "<< d[1][1]->m_weight <<" "<< d[2][1]->m_weight << endl <<endl;
 	return 0;
 }
--- Utils/Graph/include/graph.hpp
+++ Utils/Graph/include/graph.hpp
@@ -79,4 +79,153 @@ public:
 	}
 };
 
+template <typename nodeProperties, typename edgeProperties,
+		  typename edgeContainer,
+		  typename nodeDescriptor,
+		  typename edgeDescriptor>
+class nodeWithEdges <nodeProperties*, edgeProperties*, edgeContainer, nodeDescriptor, edgeDescriptor>
+{
+public:
+	nodeProperties *m_nodeProperties;
+	typedef typename container_gen<edgeContainer,edgeProperties*>::type edgeListType;
+	edgeListType m_edgeList;
+	typedef nodeDescriptor nodeDescriptorType;
+	nodeDescriptorType m_nodeDescriptor;
+	typedef edgeDescriptor edgeDescriptorType;
+	edgeDescriptorType m_edgeDescriptor;
+	nodeWithEdges():m_nodeProperties(NULL),m_edgeList(0){};
+	nodeWithEdges(nodeProperties* node):m_edgeList(0) {this->m_nodeProperties = node;}
+	nodeProperties* operator[] (nodeDescriptor &v) {return this->m_nodeProperties;}
+	edgeProperties* operator[] (size_t edgeIdx) {return this->m_edgeList[edgeIdx-1];}
+	nodeDescriptor& getNodeDescriptor () {return (this->m_nodeDescriptor);}
+	edgeDescriptor& getEdgeDescriptor () {return (this->m_edgeDescriptor);}
+	void addEdge(edgeProperties* edge) {this->m_edgeList.push_back(edge);}
+	friend ostream& operator << (ostream& out, nodeWithEdges& node)
+	{
+		typename edgeListType::iterator edgeItr;
+		for (edgeItr = node.m_edgeList.begin(); edgeItr != node.m_edgeList.end() ; edgeItr++) out << *(*(edgeItr));
+		return out;
+	}
+};
+
+template <typename nodeProperties, typename edgeProperties, typename nodeContainer ,
+		  typename edgeContainer ,typename nodeDescriptor,typename edgeDescriptor >
+class graph<nodeProperties*, edgeProperties*, nodeContainer, edgeContainer, nodeDescriptor, edgeDescriptor>
+{
+public:
+	typedef nodeWithEdges<nodeProperties*, edgeProperties*, edgeContainer> nodeType;
+	typedef typename container_gen<nodeContainer,nodeType>::type nodeListType;
+	nodeListType m_nodeList;
+	typedef nodeDescriptor nodeDescriptorType;
+	typedef edgeDescriptor edgeDescriptorType;
+	graph(){};
+	graph(size_t noOfNodes) : m_nodeList(noOfNodes){};
+	nodeType& operator[](size_t idx) {return this->m_nodeList[idx-1];}
+	friend ostream& operator<< (ostream& out, graph& graphInst)
+	{
+		typename nodeListType::iterator nodeItr;
+		for (nodeItr = graphInst.m_nodeList.begin(); nodeItr != graphInst.m_nodeList.end() ;nodeItr++) out << *(nodeItr);
+		return out;
+	}
+};
+
+template <typename nodeProperties, typename edgeProperties,
+		  typename edgeContainer,
+		  typename nodeDescriptor,
+		  typename edgeDescriptor>
+class nodeWithEdges <nodeProperties*, edgeProperties, edgeContainer, nodeDescriptor, edgeDescriptor>
+{
+public:
+	nodeProperties *m_nodeProperties;
+	typedef typename container_gen<edgeContainer,edgeProperties>::type edgeListType;
+	edgeListType m_edgeList;
+	typedef nodeDescriptor nodeDescriptorType;
+	nodeDescriptorType m_nodeDescriptor;
+	typedef edgeDescriptor edgeDescriptorType;
+	edgeDescriptorType m_edgeDescriptor;
+	nodeWithEdges():m_nodeProperties(NULL),m_edgeList(0){};
+	nodeWithEdges(nodeProperties* node):m_edgeList(0) {this->m_nodeProperties = node;}
+	nodeProperties* operator[] (nodeDescriptor &v) {return this->m_nodeProperties;}
+	edgeProperties& operator[] (size_t edgeIdx) {return this->m_edgeList[edgeIdx-1];}
+	nodeDescriptor& getNodeDescriptor () {return (this->m_nodeDescriptor);}
+	edgeDescriptor& getEdgeDescriptor () {return (this->m_edgeDescriptor);}
+	void addEdge(edgeProperties &edge) {this->m_edgeList.push_back(edge);}
+	friend ostream& operator << (ostream& out, nodeWithEdges& node)
+	{
+		typename edgeListType::iterator edgeItr;
+		for (edgeItr = node.m_edgeList.begin(); edgeItr != node.m_edgeList.end() ; edgeItr++) out << *(edgeItr);
+		return out;
+	}
+};
+
+template <typename nodeProperties, typename edgeProperties, typename nodeContainer ,
+		  typename edgeContainer ,typename nodeDescriptor,typename edgeDescriptor >
+class graph<nodeProperties*, edgeProperties, nodeContainer, edgeContainer, nodeDescriptor, edgeDescriptor>
+{
+public:
+	typedef nodeWithEdges<nodeProperties*, edgeProperties, edgeContainer> nodeType;
+	typedef typename container_gen<nodeContainer,nodeType>::type nodeListType;
+	nodeListType m_nodeList;
+	typedef nodeDescriptor nodeDescriptorType;
+	typedef edgeDescriptor edgeDescriptorType;
+	graph(){};
+	graph(size_t noOfNodes) : m_nodeList(noOfNodes){};
+	nodeType& operator[](size_t idx) {return this->m_nodeList[idx-1];}
+	friend ostream& operator<< (ostream& out, graph& graphInst)
+	{
+		typename nodeListType::iterator nodeItr;
+		for (nodeItr = graphInst.m_nodeList.begin(); nodeItr != graphInst.m_nodeList.end() ;nodeItr++) out << *(nodeItr);
+		return out;
+	}
+};
+
+template <typename nodeProperties, typename edgeProperties,
+		  typename edgeContainer,
+		  typename nodeDescriptor,
+		  typename edgeDescriptor>
+class nodeWithEdges <nodeProperties, edgeProperties*, edgeContainer, nodeDescriptor, edgeDescriptor>
+{
+public:
+	nodeProperties m_nodeProperties;
+	typedef typename container_gen<edgeContainer,edgeProperties*>::type edgeListType;
+	edgeListType m_edgeList;
+	typedef nodeDescriptor nodeDescriptorType;
+	nodeDescriptorType m_nodeDescriptor;
+	typedef edgeDescriptor edgeDescriptorType;
+	edgeDescriptorType m_edgeDescriptor;
+	nodeWithEdges():m_edgeList(0){};
+	nodeWithEdges(nodeProperties &node):m_edgeList(0) {this->m_nodeProperties = node;}
+	nodeProperties operator[] (nodeDescriptor &v) {return this->m_nodeProperties;}
+	edgeProperties* operator[] (size_t edgeIdx) {return this->m_edgeList[edgeIdx-1];}
+	nodeDescriptor& getNodeDescriptor () {return (this->m_nodeDescriptor);}
+	edgeDescriptor& getEdgeDescriptor () {return (this->m_edgeDescriptor);}
+	void addEdge(edgeProperties* edge) {this->m_edgeList.push_back(edge);}
+	friend ostream& operator << (ostream& out, nodeWithEdges& node)
+	{
+		typename edgeListType::iterator edgeItr;
+		for (edgeItr = node.m_edgeList.begin(); edgeItr != node.m_edgeList.end() ; edgeItr++) out << *(*(edgeItr));
+		return out;
+	}
+};
+
+template <typename nodeProperties, typename edgeProperties, typename nodeContainer ,
+		  typename edgeContainer ,typename nodeDescriptor,typename edgeDescriptor >
+class graph<nodeProperties, edgeProperties*, nodeContainer, edgeContainer, nodeDescriptor, edgeDescriptor>
+{
+public:
+	typedef nodeWithEdges<nodeProperties, edgeProperties*, edgeContainer> nodeType;
+	typedef typename container_gen<nodeContainer,nodeType>::type nodeListType;
+	nodeListType m_nodeList;
+	typedef nodeDescriptor nodeDescriptorType;
+	typedef edgeDescriptor edgeDescriptorType;
+	graph(){};
+	graph(size_t noOfNodes) : m_nodeList(noOfNodes){};
+	nodeType& operator[](size_t idx) {return this->m_nodeList[idx-1];}
+	friend ostream& operator<< (ostream& out, graph& graphInst)
+	{
+		typename nodeListType::iterator nodeItr;
+		for (nodeItr = graphInst.m_nodeList.begin(); nodeItr != graphInst.m_nodeList.end() ;nodeItr++) out << *(nodeItr);
+		return out;
+	}
+};
 #endif /* UTILS_GRAPH_INCLUDE_GRAPH_HPP_ */
